a5bcde7360ff82d42b492a34acd559e4c334bfeb
Lamonier Barbosa <lamoni_lr@hotmail.com>
2024-09-27 15:12:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let heroi = [
+    nome = "Devzão",
+    xp = 11000,
+    nivel = "",
+];
+
+if (heroi[1] <= 1000) {
+    heroi[2] = "Ferro"
+    console.log(`O Herói de nome ${heroi[0]} está no nível ${heroi[2]}`)
+};
+if (heroi[1] > 1000 && heroi[1] <= 2000) {
+    heroi[2] = "Bronze"
+    console.log(`O Herói de nome ${heroi[0]} está no nível ${heroi[2]}`)
+};
+if (heroi[1] > 2000 && heroi[1] <= 5000) {
+    heroi[2] = "Prata"
+    console.log(`O Herói de nome ${heroi[0]} está no nível ${heroi[2]}`)
+};
+if (heroi[1] > 5000 && heroi[1] <= 7000) {
+    heroi[2] = "Ouro"
+    console.log(`O Herói de nome ${heroi[0]} está no nível ${heroi[2]}`)
+};
+if (heroi[1] > 7000 && heroi[1] <= 8000) {
+    heroi[2] = "Platina"
+    console.log(`O Herói de nome ${heroi[0]} está no nível ${heroi[2]}`)
+};
+if (heroi[1] > 8000 && heroi[1] <= 9000) {
+    heroi[2] = "Ascendente"
+    console.log(`O Herói de nome ${heroi[0]} está no nível ${heroi[2]}`)
+};
+if (heroi[1] > 9000 && heroi[1] <= 10000) {
+    heroi[2] = "Imortal"
+    console.log(`O Herói de nome ${heroi[0]} está no nível ${heroi[2]}`)
+};
+if (heroi[1] > 10000) {
+    heroi[2] = "Radiante"
+    console.log(`O Herói de nome ${heroi[0]} está no nível ${heroi[2]}`)
+};
